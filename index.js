ec35592db88eb0357add4269990715220a44b28c
Ishimwe Pacifique <ishimweinstein@gmail.com>
2023-10-11 13:50:15 +0200

    Slight changes mad

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 //This my first JavaScript code!
 console.log("Hello Einstein!");
+/*let age = document.getElementById('head').innerHTML;
+//console.log(age);
+//let myAge =window.prompt('How old are you?');
+//let name = window.prompt('What it is your name');
+//document.getElementById('head').innerHTML= 'My name is: '+' '+ name + ' ' + 'and I am ' + myAge  + ' ' +' years old';
+//window.alert('Attention');
+let c = Math.LN10
+console.log(c); */
